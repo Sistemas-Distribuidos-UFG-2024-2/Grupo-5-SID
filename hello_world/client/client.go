5759228c30da10b8f086a16e1a2074059b56eca7
Julio Cruz <julio.cruz@mercadolivre.com>
2024-09-20 09:35:29 -0300

    atualizando client

--- hello_world/client/client.go
+++ hello_world/client/client.go
@@ -1,84 +1,60 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
-	"net/url"
+	"net"
 	"os"
 	"os/signal"
 	"time"
-
-	"github.com/gorilla/websocket"
 )
 
 func main() {
-    // Canal para capturar sinais de interrupção do sistema (como Ctrl+C).
-    interrupt := make(chan os.Signal, 1)
-    signal.Notify(interrupt, os.Interrupt)
+	// Canal para capturar sinais de interrupção do sistema (como Ctrl+C).
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
 
-    // URL do servidor WebSocket 
-    u := url.URL{Scheme: "ws", Host: "localhost:5611",  Path: "/ws"}
-    log.Printf("connecting to %s", u.String())
+	// Endereço do load balancer
+	loadBalancerAddress := "localhost:5611"
+	log.Printf("connecting to %s", loadBalancerAddress)
 
-    // Conecta ao servidor WebSocket.
-    c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-    if err != nil {
-        log.Fatal("dial:", err)
-    }
-    defer c.Close()
+	// Conecta ao load balancer via TCP
 
-    // Canal para sinalizar quando a leitura das mensagens estiver concluída.
-    done := make(chan struct{})
+	// Canal para sinalizar quando a leitura das mensagens estiver concluída.
+	done := make(chan struct{})
 
-    // Goroutine para ler mensagens do servidor WebSocket.
-    // ? funciona de forma semelhante a threads, mas são mais leves e gerenciadas pelo Go
-    go func() {
-        defer close(done)
-        for {
-            _, message, err := c.ReadMessage()
-            if err != nil {
-                log.Println("read:", err)
-                return
-            }
-            log.Printf("received: %s", message)
-        }
-    }()
+	// Ticker para enviar mensagens periodicamente.
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
 
-    // Ticker para enviar mensagens periodicamente.
-    ticker := time.NewTicker(time.Second)
-    defer ticker.Stop()
+	// Loop principal para gerenciar a comunicação com o servidor TCP.
+	for {
+		select {
+		// Caso o canal 'done' receba um valor, a função retorna, encerrando a execução.
+		case <-done:
+			return
 
-    // Loop principal para gerenciar a comunicação com o servidor WebSocket.
-    for {
-        select {
-        // Caso o canal 'done' receba um valor, a função retorna, encerrando a execução.
-        case <-done:
-            return
+		// Caso o ticker envie um valor, escrevemos uma mensagem no TCP.
+		case <-ticker.C:
+			conn, err := net.Dial("tcp", loadBalancerAddress)
+			if err != nil {
+				log.Fatal("dial:", err)
+			}
+			defer conn.Close()
 
-        // Caso o ticker envie um valor, escrevemos uma mensagem no WebSocket.
-        case t := <-ticker.C:
-            err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
-            if err != nil {
-                log.Println("write:", err)
-                return
-            }
+			_, err = fmt.Fprintf(conn, "hello\n")
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
 
-        // Caso o canal 'interrupt' receba um valor (como um sinal de interrupção), fecha a conexão WebSocket.
-        case <-interrupt:
-            log.Println("interrupt")
-
-            // Envia uma mensagem de fechamento para o WebSocket.
-            err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-            if err != nil {
-                log.Println("write close:", err)
-                return
-            }
-
-            // Espera pelo fechamento da conexão ou um timeout de 1 segundo.
-            select {
-            case <-done:
-            case <-time.After(time.Second):
-            }
-            return
-        }
-    }
+			reader := bufio.NewReader(conn)
+			message, err := reader.ReadString('\n')
+			if err != nil {
+				continue
+			}
+			log.Printf("received: %s", message)
+		}
+	}
 }
