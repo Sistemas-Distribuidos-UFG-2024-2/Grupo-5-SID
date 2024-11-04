a44cdddf8e16533eeeda67a145d28a328eb2dafb
Roberta Carvalho <roberta.carvalho@iob.com.br>
2024-11-04 16:12:19 -0300

    Porta 8080 na api do leilão

--- leilao/src/main/java/com/ufg/SID/controller/LeilaoController.java
+++ leilao/src/main/java/com/ufg/SID/controller/LeilaoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+// localhost:8080
 @RestController
 @RequestMapping("/auctions")
 public class LeilaoController {
